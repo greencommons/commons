52de5e30a7026055cae5cf7545bf42747676ed93
Pavan Trikutam <pavan@graybike.co>
2016-10-06 21:10:11 -0700

    add user model & devise settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ### CUSTOM SETTINGS ###
+
+  # Devise
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
 end
