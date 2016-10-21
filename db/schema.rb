72262b72919efea383aca6824e5f29cce57ba486
Pavan Trikutam <pavan@graybike.co>
2016-10-21 14:45:59 -0700

    add initial group migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007044258) do
+ActiveRecord::Schema.define(version: 20161021212448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",              null: false
+    t.string   "short_description"
+    t.text     "long_description"
+    t.string   "url"
+    t.string   "email"
+    t.json     "metadata"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +41,13 @@ ActiveRecord::Schema.define(version: 20161007044258) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "users_groups", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.index ["group_id"], name: "index_users_groups_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_users_groups_on_user_id", using: :btree
   end
 
   create_table "versions", force: :cascade do |t|
---
+++ spec/factories/groups.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :group do
+    name              { Faker::Company.name }
+    short_description { Faker::Lorem.sentence }
+    long_description  { Faker::Lorem.paragraph }
+    url               { Faker::Internet.url }
+  end
+end
