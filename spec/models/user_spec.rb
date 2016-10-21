4cbb1d174064a7df2074b38dd93b0279dda320aa
Pavan Trikutam <pavan@graybike.co>
2016-10-21 14:13:02 -0700

    add user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { FactoryGirl.build(:user) }
   describe 'validations' do
-    it 'is valid with valid attributes'
-    it 'is not valid without an email'
-    it 'is not valid without a password'
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
   end
 end
