4e9ef805cf9bb0b5e074df90090e6a490a1e11db
Pavan Trikutam <pavan@graybike.co>
2016-10-21 19:27:01 -0700

    adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+# Cases we should cover with seeds
+# - User with no resources, no lists, no groups
+# - User with no resources, one list, no groups
+# - User with resource, list, group
+# - User with one group, no list
+# - Group with all users, one list
+# - Group with 2 users, no list
+# - Resource belongs to a user, and two lists
+# - Resource belongs to no user, and two lists
+# - Resource belongs to one list
+# - User's list
+# - User's list
+# - Group's list
 
 # Create users
 (1..5).each do |n|
@@ -15,10 +20,19 @@
   )
 end
 
-# Create 100 fake resources
-(0..100).each do |n|
+# Create 50 fake unowned resources
+(0..50).each do |n|
   Resource.create(
     title: Faker::Book.title,
+    resource_type: Resource.resource_types.keys.sample
+  )
+end
+
+# Create 50 fake owned resources
+(0..50).each do |n|
+  Resource.create(
+    title: Faker::Book.title,
+    user: User.all.sample,
     resource_type: Resource.resource_types.keys.sample
   )
 end
@@ -47,6 +61,8 @@ g2 = Group.create(
 # Assign 2 users to group
 g2.users << User.all
 
+
+# Make lists for everyone
 User.all.each do |user|
   user.lists.create(
     name: Faker::Book.title,
@@ -61,20 +77,3 @@ end
     resources: Resource.all.sample(rand(30..50))
   )
 end
-
-# Cases we should cover with seeds
-# User with no resources, no lists, no groups
-# User with no resources, one list, no groups
-# User with resource, list, group
-# User with one group, no list
-
-# Group with all users, one list
-# Group with 2 users, no list
-
-# Resource belongs to a user, and two lists
-# Resource belongs to no user, and two lists
-# Resource belongs to one list
-
-# User's list
-# User's list
-# Group's list
