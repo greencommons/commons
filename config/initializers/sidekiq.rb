8898466be3fea4a353113bd6a83e526a799f5953
Adarsh Pandit <adarshpandit@gmail.com>
2016-11-29 19:24:43 -0800

    Correct Sidekiq configuration
    Reason for Change
    =================
    * Sidekiq jobs were not working correctly in development for a few reasons:
    1. The interface for `IndexerJob` was not being used correctly.
    1. There was no `worker` line in the `Procfiles`, so no workers were initiated. Hence, no jobs were being done.
    1. Sidekiq was not initialized to use the correct redis connection URL.
    
    Changes
    =======
    * Add configuration files and initializer for sidekiq for correct functioning.
    * Add worker directives to foreman's `Procfile` manifest.
    * Correct an interface error when creating `IndexerJob`s.
    
    Minor
    -----
    * The "hey you've disabled the Elasticsearch callbacks" warning was inverted and incorrect. Fixed here.
    * Add the sidekiq web monitoring page at `/sidekiq`, but only in development.
    
    https://trello.com/c/f0lpURT0/27-add-a-search-field-and-return-a-result

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+Sidekiq::Logging.logger = Rails.logger
+Sidekiq::Logging.logger.level = Logger::DEBUG
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
+  }
+end
