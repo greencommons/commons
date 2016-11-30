0cbe383d13cd11d6a6361491884d1c1f5b9f96fe
Adarsh Pandit <adarshpandit@gmail.com>
2016-11-29 19:24:43 -0800

    Correct Sidekiq configuration
    Reason for Change
    =================
    * Sidekiq jobs were not working correctly in development for a few reasons:
    1. The interface for `IndexerJob` was not being used correctly.
    1. There was no `worker` line in the `Procfiles`, so no workers were initiated. Hence, no jobs were being done.
    1. Sidekiq was not initialized to use the correct redis connection URL.
    
    Changes
    =======
    * Add configuration files and initializer for sidekiq for correct functioning.
    * Add worker directives to foreman's `Procfile` manifest.
    * Correct an interface error when creating `IndexerJob`s.
    
    Minor
    -----
    * The "hey you've disabled the Elasticsearch callbacks" warning was inverted and incorrect. Fixed here.
    * Add the sidekiq web monitoring page at `/sidekiq`, but only in development.
    
    https://trello.com/c/f0lpURT0/27-add-a-search-field-and-return-a-result

--- app/service/search_index.rb
+++ app/service/search_index.rb
@@ -1,23 +1,27 @@
 class SearchIndex
   def self.add(record)
     if search_index_callbacks_enabled?
-      IndexerJob.perform_async(:index, record.id)
+      IndexerJob.perform_async(:index, record.class.name, record.id)
     end
   end
 
   def self.remove(record)
     if search_index_callbacks_enabled?
-      IndexerJob.perform_async(:delete, record.id)
+      IndexerJob.perform_async(:delete, record.class.name, record.id)
     end
   end
 
   def self.search_index_callbacks_enabled?
-    Rails.logger.tagged('ELASTICSEARCH') do
-      Rails.logger.warn(
-        "Note: ElasticSearch's Model callbacks have been disabled"
-      )
+    enabled = ENV.fetch('ENABLE_SEARCH_INDEX_CALLBACKS', true) != 'false'
+
+    unless enabled
+      Rails.logger.tagged('ELASTICSEARCH') do
+        Rails.logger.warn(
+          "Note: ElasticSearch's Model callbacks have been disabled"
+        )
+      end
     end
 
-    ENV.fetch('ENABLE_SEARCH_INDEX_CALLBACKS', true) != 'false'
+    enabled
   end
 end
