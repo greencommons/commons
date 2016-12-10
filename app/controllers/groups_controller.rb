d6c83d74ade8491e46ebf9fb12b65a8c13f9c75c
T-Dnzt <thi.denizet@gmail.com>
2016-12-10 15:18:17 +0700

    Create GroupsController and Create/Update views

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,47 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to groups_path, notice: 'Group was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: 'Group was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_url, notice: 'Group was successfully destroyed.'
+  end
+
+  private
+    def set_group
+      @group = Group.find(params[:id])
+    end
+
+    def group_params
+      params.require(:group).permit(:name, :short_description, :long_description, :url, :email)
+    end
+end
---
+++ spec/requests/groups_controller_spec.rb
@@ -0,0 +1,108 @@
+require 'rails_helper'
+
+RSpec.describe 'Groups', type: :request do
+
+  let(:group) { create(:group) }
+  let(:valid_attributes) { attributes_for(:group) }
+  let(:invalid_attributes) { attributes_for(:group, name: nil) }
+
+  describe "unauthorized" do
+    describe 'GET /groups' do
+      it 'redirects to login page' do
+        get groups_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe 'authorized' do
+    before { sign_in create(:user) }
+
+    describe 'GET /groups' do
+      it 'gets 200' do
+        get groups_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'GET /groups/new' do
+      it 'gets 200' do
+        get new_group_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'GET /groups/edit' do
+      it 'gets 200' do
+        get edit_group_path(group)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'POST /groups' do
+      context 'valid attributes' do
+        it 'gets redirected to groups_path' do
+          post groups_path, params: { group: valid_attributes }
+          expect(response).to redirect_to groups_path
+        end
+
+        it 'creates the group' do
+          post groups_path, params: { group: valid_attributes }
+          expect(Group.last.name).to eq valid_attributes[:name]
+        end
+      end
+
+      context 'invalid attributes' do
+        it 'does not redirect to groups_path' do
+          post groups_path, params: { group: invalid_attributes }
+          expect(response).to have_http_status(200)
+        end
+
+        it 'does not create the group' do
+          post groups_path, params: { group: invalid_attributes }
+          expect(Group.last).to be nil
+        end
+      end
+    end
+
+    describe 'PATCH /groups/:id' do
+      let(:group) { create(:group, name: 'Nice Group') }
+
+      context 'valid attributes' do
+        it 'gets redirected to groups_path' do
+          patch group_path(group), params: { group: { name: 'Better Group' } }
+          expect(response).to redirect_to groups_path
+        end
+
+        it 'updates the group' do
+          patch group_path(group), params: { group: { name: 'Better Group' } }
+          expect(Group.last.name).to eq 'Better Group'
+        end
+      end
+
+      context 'invalid attributes' do
+        it 'does not redirect to groups_path' do
+          patch group_path(group), params: { group: { name: nil } }
+          expect(response).to have_http_status(200)
+        end
+
+        it 'does not update the group' do
+          patch group_path(group), params: { group: { name: nil } }
+          expect(Group.last.name).to eq 'Nice Group'
+        end
+      end
+    end
+
+    describe 'DELETE /groups/:id' do
+      it 'gets redirected to groups_path' do
+        delete group_path(group)
+        expect(response).to redirect_to groups_path
+      end
+
+      it 'deletes the group' do
+        delete group_path(group)
+        expect(Group.where(id: group.id).first).to be nil
+      end
+    end
+  end
+end
