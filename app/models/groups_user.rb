a41a409601347e017d91c724cd0dabc079023267
T-Dnzt <thi.denizet@gmail.com>
2016-12-10 14:32:44 +0700

    Implement the GroupsUser model linking Group and User

---
+++ app/models/groups_user.rb
@@ -0,0 +1,9 @@
+class GroupsUser < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+
+  validates :group, presence: true
+  validates :user, presence: true
+  validates :admin, inclusion: { in: [ true, false ] }
+  validates :user_id, uniqueness: { scope: :group_id }
+end
