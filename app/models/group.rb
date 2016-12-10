e62f6d72a2155fb649c954e29c312167ae0b8d57
T-Dnzt <thi.denizet@gmail.com>
2016-12-10 14:32:44 +0700

    Implement the GroupsUser model linking Group and User

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ApplicationRecord
   has_paper_trail
   acts_as_taggable
 
-  has_and_belongs_to_many :users
+  has_many :groups_users
+  has_many :users, through: :groups_users
   has_many :lists, as: :owner
 
   validates :name, presence: true
