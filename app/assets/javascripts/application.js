8a478af093d79d0bb4f9a507b60cc8535ff012c8
T-Dnzt <thi.denizet@gmail.com>
2016-12-14 15:32:16 +0700

    Add the Fetch library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,7 @@
+//= require fetch
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require react
 //= require react_ujs
 //= require components
-
-//= require jquery.easy-autocomplete
-//= require autocomplete
