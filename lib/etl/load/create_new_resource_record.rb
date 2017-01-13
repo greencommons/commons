ae603e2949e743d917367f732f513c71b8f559b3
Thibault <thi.denizet@gmail.com>
2017-01-13 23:04:34 +0100

    Skip epub creation when attributes are not a hash (#105)

--- lib/etl/load/create_new_resource_record.rb
+++ lib/etl/load/create_new_resource_record.rb
@@ -1,11 +1,17 @@
 class CreateNewResourceRecord
   def write(attributes)
-    ap 'Saving...'
     @attributes = attributes
+
+    unless @attributes.is_a? Hash
+      ap 'Error: given attributes were not provided as a hash:'
+      ap @attributes
+      return
+    end
 
     if existing_record
       ap "Title already in database: #{title}"
     else
+      ap 'Saving...'
       record = Resource.create!(attributes)
 
       ap "Title: #{record.title}"
