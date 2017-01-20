41182073dd59fdd69b6c67ed832e395240d704eb
T-Dnzt <thi.denizet@gmail.com>
2017-01-20 11:47:39 +0100

    Add test for search page

--- spec/feature/user_searches_resources_spec.rb
+++ spec/feature/user_searches_resources_spec.rb
@@ -19,6 +19,36 @@ RSpec.feature 'Searching for resources', :worker, :elasticsearch do
       expect(page).to have_text(metadata[:date])
     end
 
+    scenario 'users should see updated data for a resource when searching' do
+      title = Faker::Hipster.sentence
+      newTitle = Faker::Hipster.sentence
+      metadata = { creators: 'Rachel Carson', date: Time.zone.today }
+      resource = create(:resource, title: title, metadata: metadata)
+
+      resource.title = newTitle
+      resource.save!
+
+      wait_for { Resource.search(title).results.total }.to eq(0)
+      wait_for { Resource.search(newTitle).results.total }.to eq(1)
+
+      visit new_search_path
+      within('.customer-search-form') do
+        fill_in 'query', with: title
+        click_button 'Search'
+      end
+      expect(page).not_to have_text(title)
+
+      visit new_search_path
+      within('.customer-search-form') do
+        fill_in 'query', with: newTitle
+        click_button 'Search'
+      end
+
+      expect(page).to have_text(newTitle)
+      expect(page).to have_text(metadata[:creators])
+      expect(page).to have_text(metadata[:date])
+    end
+
     scenario 'users should not see search results for deleted files' do
       title = Faker::Hipster.sentence
       metadata = { creators: 'Rachel Carson', date: Time.zone.today }
