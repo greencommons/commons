15a0b7cd2b7bac63ff1e16b766c56454fcf1906c
T-Dnzt <thi.denizet@gmail.com>
2017-01-22 17:34:16 +0700

    Display Resource metadata

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,4 +1,12 @@
 module DateHelper
+  def humanize_str_date(str)
+    begin
+      humanize_date(Date.parse(str))
+    rescue StandardError => e
+      return nil
+    end
+  end
+
   def humanize_date(date)
     date ? date.strftime('%B %d, %Y') : 'Unknown'
   end
