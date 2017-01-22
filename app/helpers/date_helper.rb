828da0ef80ad30dc608608ecd6d05fe4afb1120b
T-Dnzt <thi.denizet@gmail.com>
2017-01-22 17:47:37 +0700

    Add tests

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,13 +1,13 @@
 module DateHelper
+  def humanize_date(date)
+    date ? date.strftime('%B %d, %Y') : 'Unknown'
+  end
+
   def humanize_str_date(str)
     begin
       humanize_date(Date.parse(str))
     rescue StandardError => e
       return nil
     end
-  end
-
-  def humanize_date(date)
-    date ? date.strftime('%B %d, %Y') : 'Unknown'
   end
 end
