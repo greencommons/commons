e3e59ed97d7ec1b4857704f6b0bb3022ba3c93bd
T-Dnzt <thi.denizet@gmail.com>
2017-01-22 16:52:17 +0700

    Add Explore section

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,6 +2,8 @@ class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show]
 
   def show
+    # TODO: Switch this to suggestion mechanism once implemented
+    @suggestions = Group.limit(6)
   end
 
   private
