a0afe45c967b00303f62977d36c6182b8db1700c
Thibault <thi.denizet@gmail.com>
2017-02-03 15:46:02 +0700

    Show score as colored label (#119)

---
+++ app/helpers/score_helper.rb
@@ -0,0 +1,11 @@
+module ScoreHelper
+  def score_class(score)
+    if score > 1
+      'label-success'
+    elsif score > 0.5
+      'label-warning'
+    else
+      'label-danger'
+    end
+  end
+end
