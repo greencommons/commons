377141909d47738685b0e81d0c26f3510c4fc1b4
Pavan Trikutam <pavan@graybike.co>
2017-02-03 12:07:38 -0800

    precompile react-server.js & components.js (#126)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+
+# https://github.com/reactjs/react-rails/issues/443#issuecomment-229665070
+Rails.application.config.assets.precompile += %w( react-server.js components.js )
