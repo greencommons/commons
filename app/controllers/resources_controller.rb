833d98e20bf08d1056deb8467fae9b9e61eb46e3
T-Dnzt <thi.denizet@gmail.com>
2017-02-16 21:43:20 +0700

    Add resource ‘new’ action

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,5 @@
 class ResourcesController < ApplicationController
+  before_action :authenticate_user!, except: [:new, :create]
   before_action :set_resource, only: [:show]
 
   def show
@@ -7,10 +8,30 @@ class ResourcesController < ApplicationController
                                         limit: 6).suggest
   end
 
+  def new
+    @resource = Resource.new
+    authorize @resource
+  end
+
+  def create
+    @resource = Resource.new(resource_params)
+    authorize @resource
+
+    if @resource.save
+      redirect_to @resource, notice: 'Group was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_resource
     @resource = Resource.find(params[:id])
     authorize @resource
+  end
+
+  def resource_params
+    params.require(:resource).permit(:title)
   end
 end
