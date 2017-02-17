89d013436474834c1fc860571f213f1d61cf4516
T-Dnzt <thi.denizet@gmail.com>
2017-02-17 14:15:29 +0700

    Update member summary card with new info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe User do
     it { is_expected.to have_many(:groups) }
     it { is_expected.to have_many(:lists) }
   end
+
+  describe '#full_name' do
+    it 'builds the full_name using first_name and last_name' do
+      user = build(:user, first_name: 'John', last_name: 'Wick')
+      expect(user.full_name).to eq 'John Wick'
+    end
+  end
 end
