3e35cfb8ff5632ab50aa80c66907714d1ee77f6d
T-Dnzt <thi.denizet@gmail.com>
2017-02-17 13:49:19 +0700

    Add resource specs

--- spec/policies/resource_policy_spec.rb
+++ spec/policies/resource_policy_spec.rb
@@ -9,11 +9,36 @@ describe ResourcePolicy do
     let(:user) { nil }
 
     it { is_expected.to permit_action(:show) }
+
+    it { is_expected.to forbid_action(:index)   }
+    it { is_expected.to forbid_action(:new)     }
+    it { is_expected.to forbid_action(:create)  }
+    it { is_expected.to forbid_action(:update)  }
+    it { is_expected.to forbid_action(:edit)    }
+    it { is_expected.to forbid_action(:destroy) }
   end
 
   context 'when user' do
     let(:user) { FactoryGirl.create(:user) }
 
     it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:index)   }
+    it { is_expected.to permit_action(:new)     }
+    it { is_expected.to permit_action(:create)  }
+    it { is_expected.to forbid_action(:update)  }
+    it { is_expected.to forbid_action(:edit)    }
+    it { is_expected.to forbid_action(:destroy) }
+
+    context 'when owner' do
+      let(:resource) { FactoryGirl.create(:resource, user: user) }
+
+      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_action(:index)   }
+      it { is_expected.to permit_action(:new)     }
+      it { is_expected.to permit_action(:create)  }
+      it { is_expected.to permit_action(:update)  }
+      it { is_expected.to permit_action(:edit)    }
+      it { is_expected.to permit_action(:destroy) }
+    end
   end
 end
