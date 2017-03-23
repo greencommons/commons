0a3bf1f483e9e738be06484be4196561ff2b13e1
T-Dnzt <thi.denizet@gmail.com>
2017-03-23 21:06:58 +0700

    Refactor PresenterHelper

--- lib/yumi/base.rb
+++ lib/yumi/base.rb
@@ -17,12 +17,6 @@ module Yumi
     end
 
     def as_relationship
-      data = if @resource.respond_to?(:each)
-        @resource.map { |c| { type: @type.pluralize, id: c.id.to_s } }
-      else
-        { type: @type.pluralize, id: @resource.id.to_s }
-      end
-
       {
         data: data,
         links: {
