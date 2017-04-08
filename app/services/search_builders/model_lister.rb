ed1888be6843a165ecf29c1e4b110f930830e4c8
T-Dnzt <thi.denizet@gmail.com>
2017-04-08 22:33:17 +0700

    Add filter specs

--- app/services/search_builders/model_lister.rb
+++ app/services/search_builders/model_lister.rb
@@ -7,17 +7,27 @@ module SearchBuilders
     }.freeze
 
     def initialize(filters)
-      @filters = filters
+      @filters = filters || {}
     end
 
     def build
-      return MODEL_TYPE_FILTERS.values unless @filters&.dig(:model_types)&.any?
+      return MODEL_TYPE_FILTERS.values unless values.any?
 
-      @filters[:model_types].keys.map do |model_type|
+      values.map do |model_type|
         if MODEL_TYPE_FILTERS.keys.include?(model_type.to_sym)
           MODEL_TYPE_FILTERS[model_type.to_sym]
         end
       end
+    end
+
+    private
+
+    def values
+      @values ||= if @filters[:model_types].is_a?(String)
+                    @filters[:model_types].split(',')
+                  else
+                    @filters[:model_types]&.keys || []
+                  end
     end
   end
 end
--- spec/services/search_builders/resource_type_filter_spec.rb
+++ spec/services/search_builders/resource_type_filter_spec.rb
@@ -24,6 +24,14 @@ RSpec.describe SearchBuilders::ResourceTypeFilter do
       end
     end
 
+    context 'with filters = { resource_types: "" }' do
+      it 'returns the given es params' do
+        filter = SearchBuilders::ResourceTypeFilter.new({ resource_types: '' },
+                                                        something: 'else')
+        expect(filter.build).to eq(something: 'else')
+      end
+    end
+
     context 'with filters = { resource_types: { books: "on", articles: "on" } }' do
       it 'builds the es params with the right conditions' do
         es_params = SearchBuilders::Builder.new.es_params
@@ -31,6 +39,32 @@ RSpec.describe SearchBuilders::ResourceTypeFilter do
         filter = SearchBuilders::ResourceTypeFilter.new(
           {
             resource_types: { books: 'on', articles: 'on' }
+          },
+          es_params
+        )
+
+        expect(filter.build[:query][:bool][:filter][:bool]).to eq(
+          should: {
+            bool: {
+              minimum_should_match: 1,
+              should: [
+                { bool: { must_not: { exists: { field: 'resource_type' } } } },
+                { term: { resource_type: 'book' } },
+                { term: { resource_type: 'article' } }
+              ]
+            }
+          }
+        )
+      end
+    end
+
+    context 'with filters = { resource_types: "books,articles" } }' do
+      it 'builds the es params with the right conditions' do
+        es_params = SearchBuilders::Builder.new.es_params
+
+        filter = SearchBuilders::ResourceTypeFilter.new(
+          {
+            resource_types: 'books,articles'
           },
           es_params
         )
