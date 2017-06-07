b9fa05dc50cc81167c4b16c209f65343a4b2ed86
T-Dnzt <thi.denizet@gmail.com>
2017-06-07 23:00:52 +0700

    Fix Rubocop issue

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,3 +1,4 @@
-ActionDispatch::Request.parameter_parsers[Mime::Type.lookup('application/vnd.api+json').symbol] = lambda do |body|
+mime = Mime::Type.lookup('application/vnd.api+json').symbol
+ActionDispatch::Request.parameter_parsers[mime] = lambda do |body|
   JSON.parse(body)
 end
