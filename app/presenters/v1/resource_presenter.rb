58f8b2c5d4843620271b181b5ef7d9cf8eaa2a1a
T-Dnzt <thi.denizet@gmail.com>
2017-08-31 19:21:38 +0700

    Add metadata to resources

--- app/presenters/v1/resource_presenter.rb
+++ app/presenters/v1/resource_presenter.rb
@@ -1,12 +1,16 @@
 module V1
   class ResourcePresenter < Yumi::Base
     type 'resource'
-    attributes :title, :short_content, :url, :published_at, :private, :tags,
-               :resource_type, :created_at, :updated_at
+    attributes :title, :short_content, :resource_url, :published_at, :private, :tags,
+               :resource_type, :created_at, :updated_at, :metadata
     links :self
 
     has_many :lists
     belongs_to :user
+
+    def resource_url
+      object.url
+    end
 
     def private
       object.privacy == 'priv'
@@ -21,3 +25,4 @@ module V1
     end
   end
 end
+
