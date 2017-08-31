b52d423ff675fabc17ef6c3234217b4b6ab033fb
T-Dnzt <thi.denizet@gmail.com>
2017-08-31 17:27:23 +0700

    Add icons for all resource types

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,6 +1,6 @@
 module IconHelper
   def resource_icon(resource)
-    if resource.resource_type.in?(%w(article book report))
+    if resource.resource_type.to_sym.in?(Resource::RESOURCE_TYPES.keys)
       render "shared/icons/#{resource.resource_type}"
     else
       render 'shared/icons/unknown'
