e559dbbf7577ab27b7ac30ed38a8edf7febc344d
Nicolas Santa <nicolas55ar@gmail.com>
2017-09-11 22:24:36 -0300

    Issue #217 - Fix error display when user tries to add the same item to a list (#259)
    * Added custom message for validation of uniq items per list
      * Proper error display on AddToListComfirmation component.
    
    https://github.com/greencommons/commons/issues/217

--- spec/models/lists_item_spec.rb
+++ spec/models/lists_item_spec.rb
@@ -2,9 +2,24 @@ require 'rails_helper'
 
 RSpec.describe ListsItem do
   describe 'Validations' do
+    let(:resource) { create(:lists_item, item: item, list: list) }
+    let(:item) { create(:resource) }
+    let(:list) { create(:list) }
+    let(:uniqueness_error_message) do
+      "List \"#{item.name}\" is already in the \"#{list.name}\" list."
+    end
+
     it 'is valid with valid attributes' do
-      liste_item = build(:lists_item)
-      expect(liste_item).to be_valid
+      expect(resource).to be_valid
+    end
+
+    it { is_expected.to validate_presence_of(:list) }
+    it { is_expected.to validate_presence_of(:item) }
+
+    it 'shuould validate uniqueness' do
+      cloned_resource = resource.dup
+      expect(cloned_resource.valid?).to eq(false)
+      expect(cloned_resource.errors.full_messages.to_sentence).to eq(uniqueness_error_message)
     end
   end
 
