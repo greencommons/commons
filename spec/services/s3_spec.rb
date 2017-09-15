6e73f212540e7c3a674e9b64adf665fbf051f523
Nicolas Santa <nicolas55ar@gmail.com>
2017-09-15 14:53:42 -0300

    Issue #212 - Implement S3 signatures for S3 direct upload functionality   * S3 class lets you generate a new signature for direct upload   * S3 class lets you generate signed URL for access to a S3 object   * S3Signature#show endpoint provides a signature used for direct upload   * ResourcePathBuilder is a helper class for build object paths used for upload     file to S3
    https://github.com/greencommons/commons/issues/212

--- spec/services/s3_spec.rb
+++ spec/services/s3_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe S3, type: :service do
+<<<<<<< HEAD
   let(:s3) { S3.new }
+=======
+  let(:resource) { S3.new }
+>>>>>>> Issue #212 - Implement S3 signatures for S3 direct upload functionality
 
   describe '#signed_url' do
     let(:path) { 'test.pdf' }
     let(:signed_param) { 'X-Amz-Signature' }
     it 'returns a signed url' do
+<<<<<<< HEAD
       expect(s3.signed_url(path)).to be_include(signed_param)
+=======
+      expect(resource.signed_url(path)).to be_include(signed_param)
+>>>>>>> Issue #212 - Implement S3 signatures for S3 direct upload functionality
     end
   end
 
@@ -22,7 +30,11 @@ RSpec.describe S3, type: :service do
     end
 
     it 'returns the signature for direct upload' do
+<<<<<<< HEAD
       result = s3.generate_signature(path)
+=======
+      result = resource.generate_signature(path)
+>>>>>>> Issue #212 - Implement S3 signatures for S3 direct upload functionality
       expect(result[:acl]).to eq(signature[:acl])
       expect(result[:key]).to eq(signature[:key])
       expect(result[:mime_type]).to eq(signature[:mime_type])
@@ -35,13 +47,21 @@ RSpec.describe S3, type: :service do
     context 'when file exists' do
       let(:path) { 'test.pdf' }
 
+<<<<<<< HEAD
       it { expect(s3.file_exists?(path)).to eq(true) }
+=======
+      it { expect(resource.file_exists?(path)).to eq(true) }
+>>>>>>> Issue #212 - Implement S3 signatures for S3 direct upload functionality
     end
 
     context 'when file does not exists' do
       let(:path) { 'inexistent.pdf' }
 
+<<<<<<< HEAD
       it { expect(s3.file_exists?(path)).to eq(false) }
+=======
+      it { expect(resource.file_exists?(path)).to eq(false) }
+>>>>>>> Issue #212 - Implement S3 signatures for S3 direct upload functionality
     end
   end
 end
