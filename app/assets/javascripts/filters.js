e55290a6d76ffd6fe8a58288d0d2d5a9e452275d
Nicolas Santa <nicolas55ar@gmail.com>
2017-09-26 13:45:42 -0300

    Issue #196 - Added search bar on search page   * The search bar on the nav bar performs new search using all filters   * The search bar on the search page will keep the filter selection
    https://github.com/greencommons/commons/issues/196

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,95 +1,13 @@
-var initFilters = function() {
-  var reload = function(url) {
-    $('#ajax-loader__panel').show();
-    window.history.pushState({}, "", url);
-    $.ajax({
-      url: url,
-      type: 'GET',
-      dataType: 'script'
-    });
-  };
+(function($, window) {
+  var initFilters = function() {
+    var searchFilterCheckboxes = $('[data-filter-checkbox]');
+    var daterange = $('input[name="daterange"]');
+    var searchForm = $('form.search-remote-form');
+    var loadPanel = $('#ajax-loader__panel');
 
-  $('input[name="daterange"]').each(function() {
-    var daterange = $(this);
-    var filtering = false;
-    var start;
-    var end;
-
-    if (daterange.data('start') && daterange.data('end')) {
-      filtering = true;
-      start = moment.unix(daterange.data('start'));
-      end = moment.unix(daterange.data('end'));
-    } else {
-      start = moment().subtract(7, 'days');
-      end = moment();
-    }
-
-    daterange.daterangepicker({
-      "startDate": start,
-      "endDate": end,
-      "buttonClasses": "btn btn-sm btn-gc",
-      autoUpdateInput: false,
-      locale: {
-        format: 'MM/DD/YYYY',
-        cancelLabel: 'Clear'
-      }
-    });
-
-    if (filtering) {
-      $(this).val(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/DD/YYYY'));
-    }
-
-    daterange.on('apply.daterangepicker', function(ev, picker) {
-      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
-
+    var updateUrlWithFilters = function(){
       var url = new Uri(window.location.href);
-      url.replaceQueryParam('filters[start]', picker.startDate.unix());
-      url.replaceQueryParam('filters[end]', picker.endDate.unix());
-      reload(url);
-    });
-
-    daterange.on('cancel.daterangepicker', function(ev, picker) {
-      $(this).val('');
-
-      var url = new Uri(window.location.href);
-      url.deleteQueryParam('filters[start]');
-      url.deleteQueryParam('filters[end]');
-      reload(url);
-    });
-  });
-
-  $('[data-filter-select]').each(function() {
-    var select = $(this);
-
-    select.on('change', function(e) {
-      var url = new Uri(window.location.href);
-      url.replaceQueryParam('sort', select.val());
-      reload(url);
-    });
-  });
-
-  $('[data-filter-checkbox]').each(function() {
-    var checkbox = $(this);
-
-    checkbox.on('change', function(e) {
-      var url = new Uri(window.location.href);
-
-      var parent = checkbox.parents('li').parents('li')
-                           .children('.filters__item').find('input');
-      var list = checkbox.parents('li').children('ul');
-      var children = list.find('input[type="checkbox"]');
-
-      if (checkbox.data('indeterminate-checkbox')) {
-        children.prop('checked', checkbox.is(':checked'));
-      } else if (checkbox.data('indeterminate-checkbox-child')) {
-        if (list.find('input[type="checkbox"]:checked').length > 0) {
-          parent.prop('checked', true);
-        } else {
-          parent.prop('checked', false);
-        }
-      }
-
-      $('[data-filter-checkbox]').each(function() {
+      searchFilterCheckboxes.each(function() {
         var check = $(this);
         if (check.is(":checked") === true) {
           url.replaceQueryParam(check.attr('name'), 'on');
@@ -99,11 +17,121 @@ var initFilters = function() {
       });
 
       reload(url);
+    };
+
+    var reload = function(url) {
+      loadPanel.show();
+      window.history.pushState({}, "", url);
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'script',
+        complete: function(){ loadPanel.hide(); }
+      });
+    };
+
+    searchForm.on('submit', function(event){
+      var query = searchForm.find('input[name="query"]').val();
+      var url = new Uri(window.location.href);
+      url.replaceQueryParam('query', query);
+      reload(url);
+      event.preventDefault();
+      event.stopPropagation();
     });
+
+    $('.clear_search_filters').on('click', function(event){
+      event.preventDefault();
+      event.stopPropagation();
+      searchFilterCheckboxes.each(function() {
+        $(this).prop('checked', true);
+      });
+      updateUrlWithFilters();
+    });
+
+    daterange.each(function() {
+      var filtering = false;
+      var start;
+      var end;
+
+      if (daterange.data('start') && daterange.data('end')) {
+        filtering = true;
+        start = moment.unix(daterange.data('start'));
+        end = moment.unix(daterange.data('end'));
+      } else {
+        start = moment().subtract(7, 'days');
+        end = moment();
+      }
+
+      daterange.daterangepicker({
+        "startDate": start,
+        "endDate": end,
+        "buttonClasses": "btn btn-sm btn-gc",
+        autoUpdateInput: false,
+        locale: {
+          format: 'MM/DD/YYYY',
+          cancelLabel: 'Clear'
+        }
+      });
+
+      if (filtering) {
+        $(this).val(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/DD/YYYY'));
+      }
+
+      daterange.on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+
+        var url = new Uri(window.location.href);
+        url.replaceQueryParam('filters[start]', picker.startDate.unix());
+        url.replaceQueryParam('filters[end]', picker.endDate.unix());
+        reload(url);
+      });
+
+      daterange.on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+
+        var url = new Uri(window.location.href);
+        url.deleteQueryParam('filters[start]');
+        url.deleteQueryParam('filters[end]');
+        reload(url);
+      });
+    });
+
+    $('[data-filter-select]').each(function() {
+      var select = $(this);
+
+      select.on('change', function(e) {
+        var url = new Uri(window.location.href);
+        url.replaceQueryParam('sort', select.val());
+        reload(url);
+      });
+    });
+
+    searchFilterCheckboxes.each(function() {
+      var checkbox = $(this);
+
+      checkbox.on('change', function(e) {
+
+        var parent = checkbox.parents('li').parents('li')
+                             .children('.filters__item').find('input');
+        var list = checkbox.parents('li').children('ul');
+        var children = list.find('input[type="checkbox"]');
+
+        if (checkbox.data('indeterminate-checkbox')) {
+          children.prop('checked', checkbox.is(':checked'));
+        } else if (checkbox.data('indeterminate-checkbox-child')) {
+          if (list.find('input[type="checkbox"]:checked').length > 0) {
+            parent.prop('checked', true);
+          } else {
+            parent.prop('checked', false);
+          }
+        }
+        updateUrlWithFilters();
+      });
+    });
+  };
+
+  $(document).on('ready turbolinks:load', function () {
+    initFilters();
   });
-};
 
-$(document).on('ready turbolinks:load', function () {
-  initFilters();
-});
-
+})(jQuery, window)
