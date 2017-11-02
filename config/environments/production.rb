f67d5f78d154619f21a45b25d1325e7effbfc252
Pavan Trikutam <pavan@graybike.co>
2017-11-02 10:48:03 -0700

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ### CUSTOM SETTINGS ###
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['HOST'],
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Devise
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
